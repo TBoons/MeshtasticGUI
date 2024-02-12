430024352aa567795caef3d61b531eb68c702d85
Tim B <boonstra@gmail.com>
2024-02-12 08:11:26 -0600

    Added tabs

--- receive.py
+++ receive.py
@@ -5,11 +5,15 @@ import __main__
 
 def onReceive(packet, interface): # called when a packet arrives
     sender = packet['fromId']
+    if 'channel' in packet.keys():
+        incomingChannel = packet['channel']
+    else:
+        incomingChannel = 0
     #print(f"\nMessage from {interface.nodes[sender]['user']['longName']} ({interface.nodes[sender]['position']['latitude']}, {interface.nodes[sender]['position']['longitude']}) to {packet['toId']} at {datetime.datetime.fromtimestamp(packet['rxTime']).strftime('%Y-%m-%d %H:%M:%S')}:\n{packet['decoded']['text']}\n")
-    triggerReceive({packet['decoded']['text']},{interface.nodes[sender]['user']['longName']})
+    triggerReceive({packet['decoded']['text']},{interface.nodes[sender]['user']['longName']},incomingChannel)
 
-def triggerReceive(message, fromNode):
-    __main__.updateReceivedMessages(message, fromNode)
+def triggerReceive(message, fromNode,channel):
+    __main__.updateReceivedMessages(message, fromNode, channel)
     print(f'Incoming Message: {message} from {fromNode}')
 
 def receiverThread():
--- window.py
+++ window.py
@@ -10,6 +10,8 @@ import datetime
 
 userShortName = mesh.myNodeInfo["user"]["shortName"]
 
+#TODO - Fix outgoing message to send to correct tab
+#TODO - make figuring our Channels and Index and utility
 def send_message_to_mesh(self):
     text = entry.get()
     #sendChannelIndex = channelIndex.get()
@@ -44,10 +46,21 @@ label_incoming.pack()
 
 # Create a text area for incoming messages
 #text_area = tk.Text(root, height=5)
-text_area = tk.Text(root, height=10, width=75, state=tk.DISABLED)
+text_area = tk.Text(root, height=4, width=75, state=tk.DISABLED)
 text_area.tag_configure("right", justify="right")
 text_area.tag_configure("left", justify="left")
 text_area.pack()
+
+tabControl = ttk.Notebook(root)
+channelTabs = {}
+  
+for channel in mesh.theChans:
+    channelTabs[channel] = ttk.Frame(tabControl)
+    tabControl.add(channelTabs[channel], text =channel)
+    channelTabs[channel].text_area = tk.Text( channelTabs[channel], height=5 )
+    #channelTabs[channel].text_area.insert(tk.END, f'This is the {channel} channel')
+    channelTabs[channel].text_area.pack() 
+tabControl.pack(expand = 1, fill ="both")
 
 #USER and CHANNEL Info
 label_nodeInfo = tk.Label(root, text="User:")
@@ -66,18 +79,24 @@ channelSection=ttk.Combobox(root,values=channelNames,width=15,
 channelSection.current(0)
 channelSection.pack(side = "left")
 
-def insert_message_text(message, direction):
-    text_area.config(state=tk.NORMAL)  # Set state to normal to allow editing
-    text_area.insert(tk.END, message, direction)  # Insert new item at the end
-    text_area.config(state=tk.DISABLED)  # Set state back to disabled to make it read-only
-    text_area.yview(tk.END)  # Auto-scroll to the end
+def insert_message_text(message, direction, incomingChannel):
+    currentMessageChannel = ''
+    #todo - put this out in the open for all things to use
+    for chan in mesh.theChans:
+         if mesh.theChans[chan] == incomingChannel:
+            currentMessageChannel = chan
+            break
+    channelTabs[currentMessageChannel].text_area.config(state=tk.NORMAL)  # Set state to normal to allow editing
+    channelTabs[currentMessageChannel].text_area.insert(tk.END, message, direction)  # Insert new item at the end
+    channelTabs[currentMessageChannel].text_area.config(state=tk.DISABLED)  # Set state back to disabled to make it read-only
+    channelTabs[currentMessageChannel].text_area.yview(tk.END)  # Auto-scroll to the end
 
 def formatMessage(fromNode, message):
     return f'{fromNode}: {message}\n'
 
-def updateReceivedMessages(message, fromNode):
+def updateReceivedMessages(message, fromNode,channel):
     receivedMessage = next(iter(message))
-    insert_message_text( formatMessage(next(iter(fromNode)), receivedMessage), "right" )
+    insert_message_text( formatMessage(next(iter(fromNode)), receivedMessage), "right", channel )
 
 def function_in_thread():
     receive.receiverThread()
